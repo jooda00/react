337e2560d36a8e2033fd7fedac5e5b0faac4995c
soluxsolux <dragon2000a@naver.com>
2021-04-23 14:04:10 +0900

    [1장] react component생성
     - react 컴포넌트 (Hello) 생성

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Hello from "./Hello";
+function App() {
+  return (
+    <div>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
