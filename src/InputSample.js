049a986949540507eb6d2ab30fe0ca47d8f6571f
soluxsolux <dragon2000a@naver.com>
2021-04-27 10:40:05 +0900

    [9장] react 여러 개의 input상태 관리하기
    - 객체 형태의 상태 관리

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,37 @@
 import React, {useState} from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+       name:'',
+       nickname:''
+    });
+
+    const {name, nickname} = inputs;
+
     const onChange = (e) => {
-        setText(e.target.value);
+        const {value, name} = e.target;
+        setInputs({
+           ...inputs,
+           [name]:value
+        });
     };
-    const onReset = () => {
-        setText('');
+    const onRest = () => {
+        setInputs({
+            name: '',
+            nickname: '',
+        })
     };
-    return (
-        <div>
-            <input onChange={onChange} value={text}/>
-            <button onClick={onReset}>초기화</button>
-            <div>
-                {/*b:볼드체(글씨 굴게 강조)*/}
-                <b>값: {text}</b>
-            </div>
-        </div>
+
+    return(
+      <div>
+          <input name={"name"} placeholder={"이름"} onChange={onChange} value={name}/>
+          <input name={"nickname"} placeholder={"닉네임"} onChange={onChange} value={nickname}/>
+          <button onClick={onRest}>초기화</button>
+          <div>
+              <b>값: </b>
+              {name}({nickname})
+          </div>
+      </div>
     );
 }
 
